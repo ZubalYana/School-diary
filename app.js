4a4dda81ede5dfac40891564082720661a5cf49d
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-04-12 00:23:43 +0300

    run server

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express()
+const path = require('path')
+const PORT = 3000;
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname, 'public', 'index.html')
+})
+app.listen(PORT, ()=>{
+    console.log(`Server work on PORT: ${PORT}`);
+})
