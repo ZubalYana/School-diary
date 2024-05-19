a4c9bb5046c9be3526daa3dc0fd2a3f76506db2f
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-05-19 16:44:43 +0300

    create and connect the telegram bot

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const app = express()
 const path = require('path')
 const PORT = 3000;
 app.use(express.static(path.join(__dirname, 'public')))
+const TelegramBot = require('node-telegram-bot-api');
+const TOKEN = '6518352627:AAHArq4zdmt02-3Uec_xN0a3k08jeEHC1TM';
+const bot = new TelegramBot(TOKEN, { polling: false });
+app.post('/send', (req, res)=>{
+    const { homework } = req.body;
+    console.log(homework);
+    bot.sendMessage(1132590035, `Домашнє: ${homework}`);
+    res.sendStatus(200);
+})
 app.get('/', (req, res)=>{
     res.sendFile(__dirname, 'public', 'index.html')
 })
