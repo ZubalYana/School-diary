f271e86d73afa5811e120fe236a8d92fd1967608
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-06-27 10:15:17 +0300

    correct the bot token and display new messages

--- app.js
+++ app.js
@@ -6,18 +6,9 @@ const fs = require('fs');
 const TelegramBot = require('node-telegram-bot-api');
 
 const PORT = 3000;
-const TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN';
+const TOKEN = '6518352627:AAHArq4zdmt02-3Uec_xN0a3k08jeEHC1TM';
 const bot = new TelegramBot(TOKEN, { polling: true });
 
-let users = [];
-
-// Load users from file if it exists, otherwise create an empty file
-const usersFilePath = path.join(__dirname, 'users.json');
-if (fs.existsSync(usersFilePath)) {
-    users = JSON.parse(fs.readFileSync(usersFilePath, 'utf8'));
-} else {
-    fs.writeFileSync(usersFilePath, JSON.stringify(users));
-}
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
@@ -32,23 +23,12 @@ bot.on('message', (msg) => {
         console.log(`Added new user: ${chatId}`);
     }
 });
-
+bot.onText('/start', message => {
+    bot.sendMessage(message.chat.id, "responce");      
+});
 app.post('/send', (req, res) => {
     const { homework } = req.body;
     console.log(`Received homework to send: ${homework}`);
-
-    const sendPromises = users.map(userId =>
-        bot.sendMessage(userId, `Домашнє: ${homework}`)
-    );
-
-    Promise.all(sendPromises)
-        .then(() => {
-            res.sendStatus(200);
-        })
-        .catch((error) => {
-            console.error('Error sending message:', error);
-            res.sendStatus(500);
-        });
 });
 
 app.get('/', (req, res) => {
