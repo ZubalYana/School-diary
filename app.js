bc61d95fb74569cf6fea7362ab3d955a1bd6a803
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-07-01 10:50:18 +0300

    clean all the telegram bot code

--- app.js
+++ app.js
@@ -9,37 +9,8 @@ const PORT = 3000;
 const TOKEN = '6518352627:AAHArq4zdmt02-3Uec_xN0a3k08jeEHC1TM'; 
 const bot = new TelegramBot(TOKEN, {});
 
-const usersFilePath = path.join(__dirname, 'users.json');
-let users = [];
-
-if (fs.existsSync(usersFilePath)) {
-    users = JSON.parse(fs.readFileSync(usersFilePath, 'utf8'));
-}
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
-
-bot.onText(/\/start/, (msg) => {
-    const chatId = msg.chat.id;
-
-    if (!users.includes(chatId)) {
-        users.push(chatId);
-        fs.writeFileSync(usersFilePath, JSON.stringify(users));
-    }
-
-    bot.sendMessage(chatId, "Welcome! You've activated the bot.");
-});
-
-app.post('/send', (req, res) => {
-    console.log(req.body.homework)
-    const message = req.body.homework;
-
-    users.forEach(userId => {
-        bot.sendMessage(userId, message);
-    });
-
-    res.sendStatus(200);
-});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
