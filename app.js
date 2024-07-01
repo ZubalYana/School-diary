e8b9d79fe7a754b8eced9255fa42751d4cda239e
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-07-01 11:23:25 +0300

    write the start command test sending and abjust it's appertience

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const TelegramBot = require('node-telegram-bot-api');
-
 const PORT = 3000;
 const TOKEN = '7434998252:AAGvjoW9XAUQUbgNwN0YQs7cbsSMrooX8BA'; 
-const bot = new TelegramBot(TOKEN, {});
+const bot = new TelegramBot(TOKEN, {polling: true});
 
 const usersFilePath = path.join(__dirname, 'users.json');
 let users = [];
@@ -27,7 +26,7 @@ bot.onText(/\/start/, (msg) => {
         fs.writeFileSync(usersFilePath, JSON.stringify(users));
     }
 
-    bot.sendMessage(chatId, "Welcome! You've activated the bot.");
+    bot.sendMessage(chatId, "\n Привіт! Бот активовано. \nСюди буде регулярно приходити домашнє завдання для вашої зручності. \nПопередження: у разі перенавантаження хостингу можлива затримка повідомлення до кількох хвилин, прохання бути терплячими :)");
 });
 
 app.post('/send', (req, res) => {
